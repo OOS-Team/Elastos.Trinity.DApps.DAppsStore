c4fe3905a5972f5ab66b66efcb53da2b957e4c79
Chad Racelis <chadracelis@yahoo.com>
2019-11-16 21:55:10 -0500

    fix loader loop,show app installed

--- src/app/pages/store/main/dapps/dapp-detail/dapp-detail.page.ts
+++ src/app/pages/store/main/dapps/dapp-detail/dapp-detail.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 import { Dapp } from '../../../../../dapps.model';
 import { DappsService } from '../../../../../dapps.service';
@@ -17,9 +18,11 @@ export class DappDetailPage implements OnInit {
   dapp: Dapp;
   dappBanner = '';
   installing = false;
+  installed = false;
 
   constructor(
     private dappsService: DappsService,
+    private http: HttpClient,
     private route: ActivatedRoute,
     private navCtrl: NavController
   ) { }
@@ -38,19 +41,63 @@ export class DappDetailPage implements OnInit {
   }
 
   async installApp(dapp) {
-    this.installing = true;
     // Download the file
     const epkPath = await this.downloadAppEPK(dapp);
     console.log("EPK file downloaded and saved to " + epkPath);
-    if (epkPath) {
-      this.installing = false;
-    }
+
     // Ask the app installer to install the DApp
     appService.sendIntent("appinstall", {url: epkPath, dappStoreServerAppId: dapp._id});
   }
 
   async downloadAppEPK(dapp) {
-    return await this.dappsService.downloadDapp(dapp);
+    return await this.downloadDapp(dapp);
+  }
+
+  downloadDapp(app) {
+    console.log("App download starting...");
+    this.installing = true;
+
+    return new Promise((resolve, reject) => {
+      // Download EPK file as blob
+      this.http.get('https://dapp-store.elastos.org/apps/'+app._id+'/download', {
+        responseType: 'arraybuffer'} ).subscribe(async response => {
+        console.log("Downloaded", response);
+        let blob = new Blob([response], { type: "application/octet-stream" });
+        console.log("Blob", blob);
+        this.installing = false;
+        this.installed = true;
+
+        // Save to a temporary location
+        let filePath = await this._savedDownloadedBlobToTempLocation(blob);
+
+        resolve(filePath);
+      });
+    });
+  }
+
+  _savedDownloadedBlobToTempLocation(blob) {
+    let fileName = "appinstall.epk"
+
+    return new Promise((resolve, reject) => {
+      window.resolveLocalFileSystemURL(cordova.file.dataDirectory, (dirEntry: DirectoryEntry) => {
+          dirEntry.getFile(fileName, { create: true, exclusive: false }, (fileEntry) => {
+            console.log("Downloaded file entry", fileEntry);
+            fileEntry.createWriter((fileWriter) => {
+              fileWriter.write(blob);
+              resolve("trinity:///data/"+fileName);
+            }, (err) => {
+              console.error("createWriter ERROR - "+JSON.stringify(err));
+              reject(err);
+            });
+          }, (err) => {
+            console.error("getFile ERROR - "+JSON.stringify(err));
+            reject(err);
+          });
+      }, (err) => {
+        console.error("resolveLocalFileSystemURL ERROR - "+JSON.stringify(err));
+        reject(err);
+      });
+    });
   }
 
   closeApp() {
