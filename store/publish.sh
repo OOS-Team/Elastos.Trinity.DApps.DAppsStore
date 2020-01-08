83c0d2eb122bc619ca25f0dc72cac45691daf121
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-01-08 09:00:39 +0800

    New store version

--- store/publish.sh
+++ store/publish.sh
@@ -1,1 +1,1 @@
-trinity-cli publish --did did:elastos:iTdevVryxQcKEjXEijDV3Kzhge42A3ct95#primary --didstore ../../../../DAppSignatures/org.elastos.trinity.dapp.dappstore/appdid/
+trinity-cli publish --news "$1" --did did:elastos:iTdevVryxQcKEjXEijDV3Kzhge42A3ct95#primary --didstore ../../../../DAppSignatures/org.elastos.trinity.dapp.dappstore/appdid/
