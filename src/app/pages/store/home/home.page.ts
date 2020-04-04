355c801f07e2c0400824bf4adf7189de76f663bb
Chad Racelis <chadracelis@yahoo.com>
2020-04-03 21:39:53 -0400

    Add intent 'registerapplicationprofile' to menu-items

--- src/app/pages/store/home/home.page.ts
+++ src/app/pages/store/home/home.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ToastController } from '@ionic/angular';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { ToastController, AlertController } from '@ionic/angular';
 
 import { DappsService } from 'src/app/services/dapps.service';
 import { Dapp } from 'src/app/models/dapps.model';
@@ -22,7 +22,9 @@ export class HomePage implements OnInit {
 
   constructor(
     public dappsService: DappsService,
-    public toastController: ToastController
+    public toastController: ToastController,
+    private alertController: AlertController,
+    private zone: NgZone
   ) {
   }
 
@@ -56,6 +58,12 @@ export class HomePage implements OnInit {
     titleBarManager.setBackgroundColor("#FFFFFF");
     titleBarManager.setForegroundMode(TitleBarPlugin.TitleBarForegroundMode.DARK);
     titleBarManager.setNavigationMode(TitleBarPlugin.TitleBarNavigationMode.HOME);
+    titleBarManager.setupMenuItems([{key: 'registerApp', iconPath: '/assets/images/register.png', title: 'Register Capsule'}], this.askToRegister);
+  }
+
+  askToRegister = () => {
+    console.log('Menu item clicked');
+    this.registerAppAlert();
   }
 
   getAppIcon(app: Dapp) {
@@ -79,5 +87,33 @@ export class HomePage implements OnInit {
     return this.categories.sort((cat1, cat2) => {
       return cat2.appCount - cat1.appCount;
     });
+  }
+
+  async registerAppAlert() {
+    const alert = await this.alertController.create({
+      mode: 'ios',
+      header: 'Would you like to add Capsule Marketplace to your profile?',
+      message: 'Registering a capsule will allow your followers via Contacts to effortlessly browse your favorite capsules!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('No thanks');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            appManager.sendIntent("registerapplicationprofile", {
+              identifier: "Capsule Browser",
+              connectactiontitle: "Find the latest and greatest Capsules!"
+            }, {});
+          }
+        },
+      ]
+    });
+    alert.present();
   }
 }
