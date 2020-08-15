45ee96bb130a4d7d1c71017a6dfffbc1bdd40a4e
Noah <38987002+OOS-Team@users.noreply.github.com>
2020-08-15 16:52:11 +0200

    German translation
    German translation of the DAppsStore.

---
+++ src/assets/de.ts
@@ -0,0 +1,60 @@
+export const de = {
+  /********** Generic ***********/
+  home: "Startseite",
+  search: "Suche",
+  "new-releases": "Neuveröffentlichungen",
+  "see-all": "Alle anzeigen",
+  browse: "Durchsuchen",
+  open: "Öffnen",
+  update: "Aktualisieren",
+  install: "Installieren",
+  "search-capsule": "Kapsel suchen",
+  "no-results": "Keine Ergebnisse gefunden",
+  cats: "Kategorien",
+  capsules: "Kapseln",
+
+  /********** Categories ***********/
+  new: "Neu",
+  popular: "Beliebt",
+  finance: "Finanzen",
+  tools: "Werkzeuge",
+  communication: "Kommunikation",
+  social: "Sozial",
+  productivity: "Produktivität",
+  shopping: "Einkaufen",
+  lifestyle: "Lifestyle",
+  games: "Spiele",
+  casino: "Kasino",
+  music: "Musik",
+  entertainment: "Unterhaltung",
+  travel: "Reisen",
+  health: "Gesundheit",
+  techdemo: "Demo",
+
+  /********** Home Page ***********/
+  welcome: "Willkommen",
+  "capsule-store": "Kapsel Shop",
+  "capsule-market": "Kapsel Marktplatz",
+  capsule: "Kapsel",
+  showcase: "Schaufenster",
+  "top-cats": "Top Kategorien",
+
+  /********** Dapps Details Page ***********/
+  "capsule-info": "Kapsel Informationen",
+  description: "Beschreibung",
+  "whats-new": "Was gibt's Neues",
+  "capsule-details": "Kapsel Details",
+  cat: "Kategorie",
+  published: "Veröffentlicht",
+  version: "Version",
+  downloads: "Downloads",
+  "dev-details": "Entwickler Details",
+  author: "Autor",
+  email: "E-Mail",
+  site: "Website",
+
+  /********** Dapps Page ***********/
+  releases: "Veröffentlichungen",
+
+  /********** Categories Page ***********/
+};
